08caa9344e5e43872833da7ffd495fa0551d2997
tecasistente <127995706+tecasistente@users.noreply.github.com>
2023-07-12 10:09:52 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import dash
 from dash import html
 
 app = dash.Dash(__name__, use_pages=True)
+server=app.server
 
 app.layout=html.Div(
         dash.page_container
     )
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
